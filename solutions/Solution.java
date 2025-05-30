a12dabecb8183b041cdf0f0f834a937c15b1e29e
Bhanu Prakash <bhanuprakash9501@gmail.com>
2025-05-30 15:44:28 +0530

    changed the solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -1,33 +1,29 @@
 package solutions;
 
-class Solution {
-
-    public int[] smallerNumbersThanCurrent(int[] nums) {
+public class Solution {
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
         int len = nums.length;
         int[] hashNums = new int[101];
-
         int maxValue = 0;
 
-        for (int num : nums) {//hashing the nums elements in HashNums
+        for (int num : nums) {
             hashNums[num]++;
             maxValue = Math.max(maxValue, num);
         }
 
-        //making the hash array into a prefix Sum array
         for (int i = 1; i <= maxValue; i++) {
             hashNums[i] += hashNums[i - 1];
         }
 
-        //making the result array
         for (int i = 0; i < len; i++) {
             int indexToLook = nums[i] - 1;
             if (indexToLook == -1) {
                 nums[i] = 0;
-                continue;
+            } else {
+                nums[i] = hashNums[indexToLook];
             }
-            nums[i] = hashNums[indexToLook];
         }
-        // print ans
+
         return nums;
     }
 }
