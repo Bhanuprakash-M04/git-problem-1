8b3d640089b129b8d48dfd1d159d9553cd12f39a
Bhanu Prakash <bhanuprakash9501@gmail.com>
2025-05-30 15:38:48 +0530

    changed the solution.cpp

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -1,33 +1,33 @@
-
-package solutions; 
+package solutions;
 
 class Solution {
+
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int len = nums.length;
         int[] hashNums = new int[101];
 
         int maxValue = 0;
 
-        for(int num: nums){//hashing the nums elements in HashNums
+        for (int num : nums) {//hashing the nums elements in HashNums
             hashNums[num]++;
             maxValue = Math.max(maxValue, num);
         }
 
         //making the hash array into a prefix Sum array
-        for(int i=1; i<=maxValue; i++){
-            hashNums[i] += hashNums[i-1];
+        for (int i = 1; i <= maxValue; i++) {
+            hashNums[i] += hashNums[i - 1];
         }
 
         //making the result array
-        for(int i=0;i<len;i++){
-            int indexToLook = nums[i]-1;
-            if(indexToLook == -1){
+        for (int i = 0; i < len; i++) {
+            int indexToLook = nums[i] - 1;
+            if (indexToLook == -1) {
                 nums[i] = 0;
                 continue;
             }
             nums[i] = hashNums[indexToLook];
         }
-
+        // print ans
         return nums;
     }
 }
