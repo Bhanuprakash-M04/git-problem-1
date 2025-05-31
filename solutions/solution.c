ac28c5ab3eaf750b21829c35e5a99fd5baf6f852
Bhanu Prakash <bhanuprakash9501@gmail.com>
2025-05-31 11:38:56 +0530

    check solution.c

--- solutions/solution.c
+++ solutions/solution.c
@@ -6,24 +6,7 @@
  */
 int *smallerNumbersThanCurrent(int *nums, int numsSize, int *returnSize)
 {
-    int *res = (int *)malloc(numsSize * sizeof(int));
-    *returnSize = numsSize;
-    int hash[101] = {0};
-    for (int i = 0; i < numsSize; i++)
-    {
-        hash[nums[i]]++;
-    }
-    for (int i = 0; i < numsSize; i++)
-    {
-        int cnt = 0;
-        for (int j = 0; j < nums[i]; j++)
-        {
-            if (hash[j])
-            {
-                cnt += hash[j];
-            }
-        }
-        res[i] = cnt;
-    }
-    return res;
+
+    // write your code here
+    return 0;
 }
