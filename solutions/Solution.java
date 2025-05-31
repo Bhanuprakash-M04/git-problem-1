30b61a3d87bfb463d1a18d73a895c4195f3ee89a
Bhanu Prakash <bhanuprakash9501@gmail.com>
2025-05-31 11:59:13 +0530

    check solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -2,28 +2,6 @@ package solutions;
 
 public class Solution {
     public static int[] smallerNumbersThanCurrent(int[] nums) {
-        int len = nums.length;
-        int[] hashNums = new int[101];
-        int maxValue = 0;
-
-        for (int num : nums) {
-            hashNums[num]++;
-            maxValue = Math.max(maxValue, num);
-        }
-
-        for (int i = 1; i <= maxValue; i++) {
-            hashNums[i] += hashNums[i - 1];
-        }
-
-        for (int i = 0; i < len; i++) {
-            int indexToLook = nums[i] - 1;
-            if (indexToLook == -1) {
-                nums[i] = 0;
-            } else {
-                nums[i] = hashNums[indexToLook];
-            }
-        }
-
-        return nums;
+        return new int[0];
     }
 }
