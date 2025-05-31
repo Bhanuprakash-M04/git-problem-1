4eb0b3ff1f42a4b38e63d73b4d156ce1f49339ef
Bhanu Prakash <bhanuprakash9501@gmail.com>
2025-05-31 11:57:17 +0530

    check solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -6,19 +6,7 @@ class Solution
 public:
     vector<int> smallerNumbersThanCurrent(vector<int> &nums)
     {
-        vector<int> ans;
-        int n = nums.size();
-        for (int i = 0; i < n; i++)
-        {
-            int cnt = 0;
-            for (int j = 0; j < n; j++)
-            {
-                if (i != j && nums[i] > nums[j])
-                    cnt++;
-            }
-            ans.push_back(cnt);
-        }
-
-        return ans;
+        // write your code here
+        return {};
     }
 };
